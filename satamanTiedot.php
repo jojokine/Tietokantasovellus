7e19b5484b1a601a016a16f727bb470129508eeb
J Oskari Jokinen <jojokine@svm-19.cs.helsinki.fi>
2012-09-23 16:57:23 +0300

    sataman tietosivun toimintoja

--- satamanTiedot.php
+++ satamanTiedot.php
@@ -7,13 +7,33 @@
 
 <?php require_once('navi.php'); ?>
 
-<div id="uusisatama" style="background-color:#FFFFFF;height:600px;width:600px;float:left;">
-<h2>Nimi<h2>
-<p>käyntisatamanumero</p>
-<p>karttalehti</p>
-<p>palvelut</p>
-<p>kotisivu</p>
+<div id="satama" style="background-color:#FFFFFF;height:600px;width:600px;float:left;">
+<h2>Sataman tiedot<h2>
+ <?php 
+  include("yhteys.php");
+
+  $haku = $_POST['satama'];
+   // $haku = "Kaunissaari"
+   
+  $kysely = $yhteys->prepare("SELECT * FROM satama WHERE nimi = '$haku'");
+  $kysely->execute();
+
+	echo "<table border>";
+	while ($rivi = $kysely->fetch()) {
+	    echo "<tr>";
+	    echo "<td>" . $rivi["käyntisatamanumero"] . "</td>";
+	    echo "<td>" . $rivi["nimi"] . "</td>";
+	    echo "<td>" . $rivi["karttasivu"] . "</td>";
+	    echo "<td>" . $rivi["kiinnitys"] . "</td>";
+	    echo "<td>" . $rivi["palvelut"] . "</td>";
+	    echo "</tr>";
+	}
+//  käyntisatamanumero |    nimi     |           kiinnitys           |              palvelut               | karttasivu 
+   echo "</table>";
+ 
+?> 
 </div>
+
 
 
 </body>

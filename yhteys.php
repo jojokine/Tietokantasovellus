f735a89d3454f318e738879b6b74791e83437a19
jojokine <oskari.jokinen@helsinki.fi>
2012-09-23 16:59:43 +0300

    Update yhteys.php

--- yhteys.php
+++ yhteys.php
@@ -1,11 +1,1 @@
-<?php
 
-try {
-    $yhteys = new PDO("pgsql:host=localhost;dbname=jojokine",
-                      "jojokine", "Kissa1");
-} catch (PDOException $e) {
-    die("VIRHE: " . $e->getMessage());
-}
-$yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-?>
